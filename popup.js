38242e66ff86cb8bcd9ae08c1d9986b8212be5b0
Florian Wallner <asdf@walura.eu>
2018-06-29 21:21:31 +0200

    Input mask extended to allow any number of destinations.

--- popup.js
+++ popup.js
@@ -1,29 +1,66 @@
 load();
 
-var btn = document.getElementById("saveButton");
-btn.onclick = save;
+var saveButton = document.getElementById("saveButton");
+saveButton.onclick = saveDestinations;
+
+var addButton = document.getElementById("addButton");
+addButton.onclick = addDestination;
+
+function deleteRow() {
+	this.parentNode.parentNode.removeChild(this.parentNode);
+}
 
 function load() {
 	chrome.storage.sync.get(['names', 'destinations'], function(result) {
       console.log('Value currently is ' + result.names + result.destinations);
-      updateFields(result);
+      buildAddressList(result);
     });
-
-	
-
-
 }
 
-function updateFields(result) {
+function addDestinationToList(name, address) {
+	var destination = document.createElement("li");
+	var nameField = document.createElement("span");
+	nameField.className = "name";
+	nameField.contentEditable = "true";
+	var nameText = document.createTextNode(name);
+	nameField.appendChild(nameText);
+	destination.appendChild(nameField);
+	var addressField = document.createElement("span");
+	addressField.className = "address";
+	addressField.contentEditable = "true";
+	var addressText = document.createTextNode(address);
+	addressField.appendChild(addressText);
+	destination.appendChild(addressField);
+	var button = document.createElement("button");
+	button.innerText = "remove";
+	button.className = "deleteButton";
+	button.onclick = deleteRow;
+	destination.appendChild(button);
+	document.getElementById("address-list").appendChild(destination);
+}
+
+function buildAddressList(result) {
 	for (let i=0; i < result.names.length; ++i) {
-		document.getElementById("name"+i.toString()).innerText = result.names[i];
-		document.getElementById("address"+i.toString()).innerText = result.destinations[i];
+		addDestinationToList(result.names[i], result.destinations[i]);
 	}
 }
 
-function save() {
-	var names = [document.getElementById("name0").innerText, document.getElementById("name1").innerText];
-	var addresses = [document.getElementById("address0").innerText, document.getElementById("address1").innerText];
+function addDestination() {
+	addDestinationToList("name","address");
+}
+
+function saveDestinations() {
+	var nameElements = document.getElementsByClassName("name");
+	var names = [];
+	for (let i=0; i < nameElements.length; ++i) {
+		names.push(nameElements[i].innerText);
+	}
+
+	var addressElements = document.getElementsByClassName("address");
+	var addresses = [];
+	for (let i=0; i < addressElements.length; ++i) {
+		addresses.push(addressElements[i].innerText);
+	}
 
     // Save it using the Chrome extension storage API.
     chrome.storage.sync.set({'names': names, 'destinations': addresses}, function() {
